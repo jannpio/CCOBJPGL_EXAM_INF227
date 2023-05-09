933353965274044a303f1156a11b60ee204ebb29
Julie Ann Pioquinto <129469411+jannpio@users.noreply.github.com>
2023-05-09 12:00:24 +0800

    Create Tourist.java

---
+++ Exam/Tourist.java
@@ -0,0 +1,26 @@
+interface Tourist {
+
+    int budget = 0;
+
+    void visit();
+
+    void visit(Boracay boracay);
+
+    void visit(Cebu cebu);
+
+    void visit(Batanes batanes);
+
+    void visit(Bicol bicol);
+
+    void visit(Palawan palawan);
+
+    void visit(Sagada sagada);
+
+    default void visit(Locations locations) {
+        System.out.println("Arrived on a location");
+    };
+
+    void checkBudget();
+
+    
+}
